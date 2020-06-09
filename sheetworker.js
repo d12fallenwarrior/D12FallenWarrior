09a47f8831d296978be50a0f4a91f5429414651a
Guote <guotefac@gmail.com>
2020-06-09 21:35:34 +0200

    Ficha v1.0.0
    Creo que está todo lo básico:
    HTML
    - Transcrita la ficha entera desde el PDF.
    - Plantillas de tiradas con 2 y 3 dados, y otra más para las tiradas de daño.
    - Tiradas y atributos dependientes se calculan automáticamente. Esto incluye penalizadores de carga, pero no talentos ni conjuros.
    - Dado algo de formato básico en html que no sé si puede ser molesto después. Hay varios style="width..." en recuadros que quizás hay que quitar, así como columnas y filas dadas por roll20.
    - Hay un botón de mostrar/ocultar magia en la sección de combate que no hace nada. Quizás queremos más adelante poder hacer esa acción, pero ahora mismo puede borrarse.
    
    CSS
    - Añadida una sección al final con el formato de las plantillas de tiradas. Aparece comentado el inicio de esa sección para saber qué no borrar.

--- sheetworker.js
+++ sheetworker.js
@@ -1,32 +1,53 @@
-// HABILIDADES DE ATRIBUTO VARIABLE
-// Empatía: ast/int, Sanar: des/int, Seguridad: des/int
-on("change:des change:ast change:int sheet:opened", function () {  // stat names need to be lower case here
-    getAttrs(["des","ast","int"], function(values) {
-        let d = parseInt(values.des)||0; 
-        let a = parseInt(values.ast)||0; 
-        let i = parseInt(values.int)||0; 
-        let m_ai = Math.max(a,i);
-        let m_di = Math.max(d,i);
-    
-        setAttrs({
-            max_desint: m_di, max_astint: m_ai
+    // PLANTILLA DE 2 O 3 DADOS
+    on("change:tipotirada sheet:opened", function () {  
+        getAttrs(["tipotirada"], function(values) {
+            let tipo = parseInt(values.tipotirada)||0; 
+            let newpla = "";
+            let newr3 = "";
+            if (tipo === 3) {
+                newpla = "tres";
+                newr3 = "1d12cs0cf0";
+              } else {
+                newpla = "simple";
+                newr3 = "0";
+              };
+            setAttrs({
+                r3: newr3, plantilla: newpla
+            });
         });
     });
-});
-on("change:tipotirada sheet:opened", function () {  // stat names need to be lower case here
-    getAttrs(["tipotirada"], function(values) {
-        let tipo = parseInt(values.tipotirada)||0; 
-        let newpla = ""
-        let newr3 = ""
-        if (tipo === 3) {
-            newpla = "tres";
-            newr3 = "1d12cs0cf0";
-          } else {
-            newpla = "simple";
-            newr3 = "0";
-          }
-        setAttrs({
-            r3: newr3, plantilla: newpla
+
+    // HABILIDADES DE ATRIBUTO VARIABLE
+    // Empatía: ast/int, Sanar: des/int, Seguridad: des/int
+    on("change:des change:ast change:int sheet:opened", function () {  
+        getAttrs(["des","ast","int"], function(values) {
+            let d = parseInt(values.des)||0; 
+            let a = parseInt(values.ast)||0; 
+            let i = parseInt(values.int)||0; 
+            let m_ai = Math.max(a,i);
+            let m_di = Math.max(d,i);
+        
+            setAttrs({
+                max_desint: m_di, max_astint: m_ai
+            });
         });
     });
-});
+
+    // MODIFICADORES POR CARGA
+    on("change:carga sheet:opened", function () { 
+        getAttrs(["carga"], function(values) {
+            let c = parseInt(values.carga)||0; 
+            let p = "";
+            let b = "";
+            if (c === 999) {
+                p = "0";
+                b = "1";
+            } else {
+                p = c;
+                b = "0";
+            };
+            setAttrs({
+                penarm: p, bonocarga: b
+            });
+        });
+    });
